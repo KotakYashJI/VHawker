8ae10cdd42f445c3654a433da652572c9a15ea07
YashKotakJI <yashkotak61@gmail.com>
2025-07-29 11:16:04 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 connectdatabase();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.CLIENT_URL || "*",
     credentials: true
 }));
 
