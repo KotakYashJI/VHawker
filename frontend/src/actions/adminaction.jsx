5b095f89506a8d6e4a16e421e722c7b2acdd0ce5
Yash Kotak <yashkotak61@gmail.com>
2025-08-14 19:23:50 +0530

    last commit

--- frontend/src/actions/adminaction.jsx
+++ frontend/src/actions/adminaction.jsx
@@ -5,7 +5,7 @@ import API from "../api";
 export const loginadmin = (admin) => async (dispatch) => {
     let adminlogin;
     try {
-        adminlogin = await API.post("http://localhost:8080/api/admin/login", admin);
+        adminlogin = await API.post("/api/admin/login", admin);
         dispatch(loadloginuser(adminlogin.data.data));
         toast.success(adminlogin.data.message);
     } catch (error) {
