574f91b75dda6934928ba5d87a3b3b5334d93900
Yash Kotak <yashkotak61@gmail.com>
2025-08-21 22:51:26 +0530

    last commit

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,41 +4,6 @@ import Semiwholesalermodel from "../models/semiwholesaler.model.js";
 import Wholesalermodel from "../models/wholesaler.model.js";
 
 export const authenticateuser = async (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) return res.status(400).json({
-        message: "user not authorized please login"
-    });
-    const user = jwt.verify(token, process.env.JWT_TOKEN);
-    const usertype = user.usertype.toLowercase();
-    if (usertype === "hawker") {
-        const hawkerexist = await Hawkermodel.findOne({
-            _id: user.id
-        });
-        console.log(hawkerexist);
-
-        if (!hawkerexist) {
-            res.redirect("/#/hawker/login");
-        };
-    }
-    if (usertype === "semiwholesaler") {
-        const semiwholesalerexist = await Semiwholesalermodel.findOne({
-            _id: user.id
-        });
-        console.log(semiwholesalerexist);
-        if (!semiwholesalerexist) {
-            res.redirect("/#/semiwholealer/login");
-        };
-        next();
-    }
-    else {
-        const wholesalerexist = await Wholesalermodel.findOne({
-            _id: user.id
-        });
-        console.log(wholesalerexist);
-        if (!wholesalerexist) {
-            res.redirect("/#/wholesaler/login");
-        };
-        next();
-    }
+    const token = req;
     console.log(token);
 }
